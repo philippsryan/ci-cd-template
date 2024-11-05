f3369856b2a53e5c8bde715c39a18c16cd32557c
philippsryan <ryan.philipps@catalyst3.io>
2024-11-05 16:03:04 -0700

    change url for request to be more dynamic

--- frontend/src/todoApi.ts
+++ frontend/src/todoApi.ts
@@ -1,43 +1,46 @@
-
-
 export type Todo = {
-  id?: number,
-  belongs_to: string,
-  title: string,
-  body: string,
-  done: boolean
-}
-
+  id?: number;
+  belongs_to: string;
+  title: string;
+  body: string;
+  done: boolean;
+};
 
 export const getTodos = async (user: string) => {
   try {
     const params = new URLSearchParams();
     params.append("user", user);
-    const response = await fetch("http://localhost:8000/todos?" + params, {
-      method: "GET",
-    });
+    const response = await fetch(
+      `http://${window.location.hostname}:8000/todos?` + params,
+      {
+        method: "GET",
+      },
+    );
 
     return await response.json();
   } catch (e) {
     console.log(e);
     throw e;
   }
-}
+};
 
 export const createTodo = async (todo: Todo) => {
   try {
-    const response = await fetch("http://localhost:8000/todos", {
-      method: "POST",
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
+    const response = await fetch(
+      `http://${window.location.hostname}:8000/todos`,
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(todo),
       },
-      body: JSON.stringify(todo)
-    });
+    );
 
     return await response.json();
   } catch (e) {
     console.log(e);
     throw e;
   }
-}
+};
--- frontend/src/userApi.ts
+++ frontend/src/userApi.ts
@@ -1,34 +1,34 @@
-
-
-
-
-
 export const getUsers = async () => {
   try {
-    const response = await fetch("http://localhost:8000/user", {
-      method: 'GET'
-    });
+    const response = await fetch(
+      `http://${window.location.hostname}:8000/user`,
+      {
+        method: "GET",
+      },
+    );
 
-    return await response.json()
+    return await response.json();
   } catch (e) {
     console.log(e);
     throw e;
   }
-}
-
+};
 
 export const createUser = async (username: string) => {
   try {
-    const response = await fetch("http://localhost:8000/user", {
-      method: 'POST',
-      body: new URLSearchParams({
-        "username": username
-      })
-    });
+    const response = await fetch(
+      `http://${window.location.hostname}:8000/user`,
+      {
+        method: "POST",
+        body: new URLSearchParams({
+          username: username,
+        }),
+      },
+    );
 
-    return await response.json()
+    return await response.json();
   } catch (e) {
     console.log(e);
     throw e;
   }
-}
+};
